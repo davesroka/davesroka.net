c44e0f2f98228546097421000fd73e6c7f3d9c6f
Dave Sroka <dave@davesroka.net>
2019-02-15 13:32:36 -0500

    Add prismic as source

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,20 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`
+});
+
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: "Gatsby Default Starter"
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    "gatsby-plugin-react-helmet",
+    {
+      resolve: `gatsby-source-prismic`,
+      options: {
+        repositoryName: `davesrokanet`,
+        accessToken: `${process.env.API_KEY}`,
+        linkResolver: ({ node, key, value }) => post => `/${post.uid}`
+      }
+    }
+  ]
 };
